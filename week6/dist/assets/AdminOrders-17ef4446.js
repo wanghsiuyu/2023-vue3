35ceaf0cd4c860d364274fac934b0d5e7424cfec
Grace Wang <t4_23@yahoo.com.tw>
2023-03-01 22:47:43 +0800

    fix vite.config.js & push dist folder

---
+++ week6/dist/assets/AdminOrders-17ef4446.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-8d8e4631.js";const e={};function c(n,t){return"訂單列表"}const s=r(e,[["render",c]]);export{s as default};
---
+++ week6/dist/assets/AdminProducts-506a7a7f.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index-8d8e4631.js";const c={};function t(e,n){return"產品列表"}const o=r(c,[["render",t]]);export{o as default};
